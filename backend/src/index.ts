5c7ceefcd34b446a3165880e5c9983bf63c4cdc3
Jesús Manuel Bañuelos Ibarra <banuelosibarrajesusmanuel@gmail.com>
2024-04-24 20:29:07 -0700

    Add cloudinary and multer dependencies, and update routes in backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,15 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
+import myHotelsRoutes from './routes/my-hotels';
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(cookieParser());
@@ -20,6 +28,7 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelsRoutes);
 
 mongoose.connect(process.env.MONGO_URL as string)
     .then(() => {
