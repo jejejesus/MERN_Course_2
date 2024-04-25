d1886f17c446e826a74e3f96f9eedd3b3d41f24e
Jesús Manuel Bañuelos Ibarra <banuelosibarrajesusmanuel@gmail.com>
2024-04-24 18:33:28 -0700

    Update .gitignore and package.json, and add static file serving in backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
+import path from 'path';
 
 const app = express();
 app.use(cookieParser());
@@ -14,6 +15,8 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
  }));
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
