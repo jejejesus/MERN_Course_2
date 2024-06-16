2b17e1719ae166b8755fd6c815b3eab298462bfa
Jesús Manuel Bañuelos Ibarra <banuelosibarrajesusmanuel@gmail.com>
2024-06-15 18:00:03 -0700

    chore: Add booking functionality to hotel model and routes

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
-import { HotelType } from "../shared/types";
+import { BookingType, HotelType } from "../shared/types";
+
+const bookingSchema = new mongoose.Schema<BookingType>(
+  {
+    userId: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    adultCount: { type: Number, required: true },
+    childCount: { type: Number, required: true },
+    checkIn: { type: Date, required: true },
+    checkOut: { type: Date, required: true },
+    totalCost: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
 const hotelSchema = new mongoose.Schema<HotelType>(
   {
@@ -16,6 +31,7 @@ const hotelSchema = new mongoose.Schema<HotelType>(
     starRating: { type: Number, required: true, min: 1, max: 5 },
     imageUrls: [{ type: String, required: true }],
     lastUpdated: { type: Date, required: true },
+    bookings: [bookingSchema],
   },
   { timestamps: true }
 );
